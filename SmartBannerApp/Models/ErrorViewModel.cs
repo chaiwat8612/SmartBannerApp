5e3eecaa08841311ae2899b0fc7b985b059b879a
chaiwat8612 <89202968+chaiwat8612@users.noreply.github.com>
2022-05-13 15:48:53 +0700

    Add require files

---
+++ SmartBannerApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SmartBannerApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
